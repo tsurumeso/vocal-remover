3e84e06767265d158d505260c8167695e2509066
tsurumeso <tsurumeso@gmail.com>
2020-05-02 00:03:56 +0900

    Skip already augmented files

--- augment.py
+++ augment.py
@@ -15,18 +15,18 @@ if __name__ == '__main__':
     p.add_argument('--sr', '-r', type=int, default=44100)
     p.add_argument('--hop_length', '-l', type=int, default=1024)
     p.add_argument('--pitch', '-p', type=int, default=-2)
-    p.add_argument('--mixture_dataset', '-m', required=True)
-    p.add_argument('--instrumental_dataset', '-i', required=True)
+    p.add_argument('--mixtures', '-m', required=True)
+    p.add_argument('--instruments', '-i', required=True)
     args = p.parse_args()
 
     input_exts = ['.wav', '.m4a', '.3gp', '.oma', '.mp3', '.mp4']
     X_list = sorted([
-        os.path.join(args.mixture_dataset, fname)
-        for fname in os.listdir(args.mixture_dataset)
+        os.path.join(args.mixtures, fname)
+        for fname in os.listdir(args.mixtures)
         if os.path.splitext(fname)[1] in input_exts])
     y_list = sorted([
-        os.path.join(args.instrumental_dataset, fname)
-        for fname in os.listdir(args.instrumental_dataset)
+        os.path.join(args.instruments, fname)
+        for fname in os.listdir(args.instruments)
         if os.path.splitext(fname)[1] in input_exts])
 
     input_i = 'input_i_{}.wav'.format(args.pitch)
@@ -39,6 +39,13 @@ if __name__ == '__main__':
 
     filelist = list(zip(X_list, y_list))
     for mix_path, inst_path in tqdm(filelist):
+        basename_mix, _ = os.path.splitext(os.path.basename(mix_path))
+        basename_inst, _ = os.path.splitext(os.path.basename(inst_path))
+        outpath_mix = os.path.join(args.mixtures, basename_mix + suffix)
+        outpath_inst = os.path.join(args.instruments, basename_inst + suffix)
+        if os.path.exists(outpath_mix) and os.path.exists(outpath_inst):
+            continue
+
         X, _ = librosa.load(
             mix_path, args.sr, False, dtype=np.float32, res_type='kaiser_fast')
         y, _ = librosa.load(
@@ -61,14 +68,10 @@ if __name__ == '__main__':
         X = y + v
 
         spec = spec_utils.calc_spec(X, args.hop_length)
-        basename, _ = os.path.splitext(os.path.basename(mix_path))
-        outpath = os.path.join(args.mixture_dataset, basename + suffix)
-        np.save(outpath, np.abs(spec))
+        np.save(outpath_mix, np.abs(spec))
 
         spec = spec_utils.calc_spec(y, args.hop_length)
-        basename, _ = os.path.splitext(os.path.basename(inst_path))
-        outpath = os.path.join(args.instrumental_dataset, basename + suffix)
-        np.save(outpath, np.abs(spec))
+        np.save(outpath_inst, np.abs(spec))
 
         os.remove(input_i)
         os.remove(input_v)
