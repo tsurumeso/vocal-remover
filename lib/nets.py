b3f1d60a9934428420ea86c438ede274a57ebe6d
tsurumeso <tsurumeso@gmail.com>
2020-07-03 00:27:39 +0900

    Update model

--- lib/nets.py
+++ lib/nets.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+import torch.nn.functional as F
 
 from lib import layers
 
@@ -38,47 +39,75 @@ class BaseASPPNet(nn.Module):
 
 class CascadedASPPNet(nn.Module):
 
-    def __init__(self):
+    def __init__(self, n_fft, mean=None, std=None):
         super(CascadedASPPNet, self).__init__()
-        self.low_band_net = BaseASPPNet(2, 32, ((2, 4), (4, 8), (8, 16)))
-        self.high_band_net = BaseASPPNet(2, 32, ((2, 4), (4, 8), (8, 16)))
+        self.low_band_net = BaseASPPNet(2, 16)
+        self.high_band_net = BaseASPPNet(2, 16)
+        self.diff_net = BaseASPPNet(1, 16)
 
         self.bridge = layers.Conv2DBNActiv(34, 16, 1, 1, 0)
         self.full_band_net = BaseASPPNet(16, 32)
 
-        self.out = nn.Sequential(
-            layers.Conv2DBNActiv(32, 16, 3, 1, 1),
-            nn.Conv2d(16, 2, 1, bias=False))
+        self.out = nn.Conv2d(32, 2, 1, bias=False)
         self.aux_out = nn.Conv2d(32, 2, 1, bias=False)
+
+        self.max_bin = n_fft // 2
+        self.output_bin = n_fft // 2 + 1
+
+        if mean is None:
+            mean = torch.zeros(self.output_bin, 1)
+        else:
+            mean = torch.from_numpy(mean[:, None])
+
+        if std is None:
+            std = torch.ones(self.output_bin, 1)
+        else:
+            std = torch.from_numpy(std[:, None])
+
+        self.mean = nn.Parameter(mean)
+        self.std = nn.Parameter(std)
 
         self.offset = 128
 
-    def __call__(self, x):
+    def forward(self, x):
+        mix = x.detach()
+        x = x.clone()
+
+        x -= self.mean
+        x /= self.std
+
+        x = x[:, :, :self.max_bin]
+
         bandw = x.size()[2] // 2
         aux = torch.cat([
-            self.low_band_net(x[:, :, :bandw]),
-            self.high_band_net(x[:, :, bandw:])
-        ], dim=2)
+            torch.cat([
+                self.low_band_net(x[:, :, :bandw]),
+                self.high_band_net(x[:, :, bandw:])
+            ], dim=2),
+            self.diff_net(x[:, 0, None] - x[:, 1, None])
+        ], dim=1)
 
         h = torch.cat([x, aux], dim=1)
         h = self.full_band_net(self.bridge(h))
+        h = torch.tanh(self.out(h)) + 1
+        h = F.pad(
+            input=h,
+            pad=(0, 0, 0, self.output_bin - h.size()[2]),
+            mode='replicate')
 
-        h = torch.sigmoid(self.out(h))
-        aux = torch.sigmoid(self.aux_out(aux))
+        if self.training:
+            aux = torch.tanh(self.aux_out(aux)) + 1
+            aux = F.pad(
+                input=aux,
+                pad=(0, 0, 0, self.output_bin - aux.size()[2]),
+                mode='replicate')
+            return h * mix, aux * mix
+        else:
+            return h * mix
 
-        return h, aux
+    def predict(self, x_mag):
+        h = self.forward(x_mag)
 
-    def predict(self, x):
-        bandw = x.size()[2] // 2
-        aux = torch.cat([
-            self.low_band_net(x[:, :, :bandw]),
-            self.high_band_net(x[:, :, bandw:])
-        ], dim=2)
-
-        h = torch.cat([x, aux], dim=1)
-        h = self.full_band_net(self.bridge(h))
-
-        h = torch.sigmoid(self.out(h))
         if self.offset > 0:
             h = h[:, :, :, self.offset:-self.offset]
             assert h.size()[3] > 0
--- lib/spec_utils.py
+++ lib/spec_utils.py
@@ -3,27 +3,27 @@ import os
 import librosa
 import numpy as np
 import soundfile as sf
-import torch
 
 
-def crop_center(h1, h2, concat=True):
-    # s_freq = (h2.shape[2] - h1.shape[2]) // 2
-    # e_freq = s_freq + h1.shape[2]
+def crop_center(h1, h2):
     h1_shape = h1.size()
     h2_shape = h2.size()
-    if h2_shape[3] < h1_shape[3]:
-        raise ValueError('h2_shape[3] must be greater than h1_shape[3]')
-    s_time = (h2_shape[3] - h1_shape[3]) // 2
-    e_time = s_time + h1_shape[3]
-    h2 = h2[:, :, :, s_time:e_time]
-    if concat:
-        return torch.cat([h1, h2], dim=1)
-    else:
-        return h2
+
+    if h1_shape[3] == h2_shape[3]:
+        return h1
+    elif h1_shape[3] < h2_shape[3]:
+        raise ValueError('h1_shape[3] must be greater than h2_shape[3]')
+
+    # s_freq = (h2_shape[2] - h1_shape[2]) // 2
+    # e_freq = s_freq + h1_shape[2]
+    s_time = (h1_shape[3] - h2_shape[3]) // 2
+    e_time = s_time + h2_shape[3]
+    h1 = h1[:, :, :, s_time:e_time]
+
+    return h1
 
 
-def calc_spec(X, hop_length):
-    n_fft = (hop_length - 1) * 2
+def get_spectrogram(X, hop_length, n_fft):
     audio_left = np.asfortranarray(X[0])
     audio_right = np.asfortranarray(X[1])
     spec_left = librosa.stft(audio_left, n_fft, hop_length=hop_length)
@@ -31,6 +31,32 @@ def calc_spec(X, hop_length):
     spec = np.asfortranarray([spec_left, spec_right])
 
     return spec
+
+
+def spectrogram_to_image(spec, mode='magnitude'):
+    if mode == 'magnitude':
+        if np.iscomplexobj(spec):
+            y = np.abs(spec)
+        else:
+            y = spec
+        y = np.log10((y) ** 2 + 1e-8)
+    elif mode == 'phase':
+        if np.iscomplexobj(spec):
+            y = np.angle(spec)
+        else:
+            y = spec
+
+    y -= y.min()
+    y *= 255 / y.max()
+    img = np.uint8(y)
+
+    if y.ndim == 3:
+        img = img.transpose(1, 2, 0)
+        img = np.concatenate([
+            np.max(img, axis=2, keepdims=True), img
+        ], axis=2)
+
+    return img
 
 
 def mask_uninformative(mask, ref, thres=0.3, min_range=64, fade_area=32):
@@ -64,10 +90,15 @@ def mask_uninformative(mask, ref, thres=0.3, min_range=64, fade_area=32):
 
 
 def align_wave_head_and_tail(a, b, sr):
+    a, _ = librosa.effects.trim(a)
+    b, _ = librosa.effects.trim(b)
+
     a_mono = a[:, :sr * 4].sum(axis=0)
     b_mono = b[:, :sr * 4].sum(axis=0)
+
     a_mono -= a_mono.mean()
     b_mono -= b_mono.mean()
+
     offset = len(a_mono) - 1
     delay = np.argmax(np.correlate(a_mono, b_mono, 'full')) - offset
 
@@ -75,6 +106,7 @@ def align_wave_head_and_tail(a, b, sr):
         a = a[:, delay:]
     else:
         b = b[:, np.abs(delay):]
+
     if a.shape[1] < b.shape[1]:
         b = b[:, :a.shape[1]]
     else:
@@ -83,11 +115,18 @@ def align_wave_head_and_tail(a, b, sr):
     return a, b
 
 
-def cache_or_load(mix_path, inst_path, sr, hop_length):
-    _, mix_ext = os.path.splitext(mix_path)
-    _, inst_ext = os.path.splitext(inst_path)
-    spec_mix_path = mix_path.replace(mix_ext, '.npy')
-    spec_inst_path = inst_path.replace(inst_ext, '.npy')
+def cache_or_load(mix_path, inst_path, sr, hop_length, n_fft):
+    mix_basename = os.path.splitext(os.path.basename(mix_path))[0]
+    inst_basename = os.path.splitext(os.path.basename(inst_path))[0]
+
+    outdir = 'sr{}_hl{}_nf{}'.format(sr, hop_length, n_fft)
+    mix_dir = os.path.join(os.path.dirname(mix_path), outdir)
+    inst_dir = os.path.join(os.path.dirname(inst_path), outdir)
+    os.makedirs(mix_dir, exist_ok=True)
+    os.makedirs(inst_dir, exist_ok=True)
+
+    spec_mix_path = os.path.join(mix_dir, mix_basename + '.npy')
+    spec_inst_path = os.path.join(inst_dir, inst_basename + '.npy')
 
     if os.path.exists(spec_mix_path) and os.path.exists(spec_inst_path):
         X = np.load(spec_mix_path)
@@ -97,12 +136,11 @@ def cache_or_load(mix_path, inst_path, sr, hop_length):
             mix_path, sr, False, dtype=np.float32, res_type='kaiser_fast')
         y, _ = librosa.load(
             inst_path, sr, False, dtype=np.float32, res_type='kaiser_fast')
-        X, _ = librosa.effects.trim(X)
-        y, _ = librosa.effects.trim(y)
+
         X, y = align_wave_head_and_tail(X, y, sr)
 
-        X = np.abs(calc_spec(X, hop_length))
-        y = np.abs(calc_spec(y, hop_length))
+        X = get_spectrogram(X, hop_length, n_fft)
+        y = get_spectrogram(y, hop_length, n_fft)
 
         _, ext = os.path.splitext(mix_path)
         np.save(spec_mix_path, X)
@@ -111,26 +149,46 @@ def cache_or_load(mix_path, inst_path, sr, hop_length):
     return X, y
 
 
-def spec_to_wav(mag, phase, hop_length):
-    spec = mag * phase
+def spectrogram_to_wave(spec, hop_length=1024):
     spec_left = np.asfortranarray(spec[0])
     spec_right = np.asfortranarray(spec[1])
-    wav_left = librosa.istft(spec_left, hop_length=hop_length)
-    wav_right = librosa.istft(spec_right, hop_length=hop_length)
-    wav = np.asfortranarray([wav_left, wav_right])
 
-    return wav
+    wave_left = librosa.istft(spec_left, hop_length=hop_length)
+    wave_right = librosa.istft(spec_right, hop_length=hop_length)
+    wave = np.asfortranarray([wave_left, wave_right])
+
+    return wave
 
 
 if __name__ == "__main__":
+    import cv2
     import sys
+
     X, _ = librosa.load(
         sys.argv[1], 44100, False, dtype=np.float32, res_type='kaiser_fast')
     y, _ = librosa.load(
         sys.argv[2], 44100, False, dtype=np.float32, res_type='kaiser_fast')
-    X, _ = librosa.effects.trim(X)
-    y, _ = librosa.effects.trim(y)
+
     X, y = align_wave_head_and_tail(X, y, 44100)
-    sf.write('test_i.wav', y.T, 44100)
-    sf.write('test_m.wav', X.T, 44100)
+
+    X_spec = get_spectrogram(X, 1024, 2048)
+    y_spec = get_spectrogram(y, 1024, 2048)
+    v_spec = X_spec - y_spec
+
+    X_mag = np.abs(X_spec)
+    y_mag = np.abs(y_spec)
+    v_mag = np.abs(v_spec)
+
+    # v_mag = v_mag * (v_mag > y_mag)
+
+    X_image = spectrogram_to_image(X_mag)
+    y_image = spectrogram_to_image(y_mag)
+    v_image = spectrogram_to_image(v_mag)
+
+    cv2.imwrite('test_y.jpg', y_image)
+    cv2.imwrite('test_X.jpg', X_image)
+    cv2.imwrite('test_v.jpg', v_image)
+
+    sf.write('test_y.wav', y.T, 44100)
+    sf.write('test_X.wav', X.T, 44100)
     sf.write('test_v.wav', (X - y).T, 44100)
