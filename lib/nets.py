faebea29d75b4a7bcceafd523dfe64d9c717efa2
tsurumeso <tsurumeso@gmail.com>
2022-01-27 23:41:56 +0900

    Update model

--- lib/nets.py
+++ lib/nets.py
@@ -7,7 +7,7 @@ from lib import layers
 
 class BaseNet(nn.Module):
 
-    def __init__(self, nin, nout, min_size, nout_lstm, dilations=((4, 2), (8, 4), (12, 6))):
+    def __init__(self, nin, nout, nin_lstm, nout_lstm, dilations=((4, 2), (8, 4), (12, 6))):
         super(BaseNet, self).__init__()
         self.enc1 = layers.Conv2DBNActiv(nin, nout, 3, 1, 1)
         self.enc2 = layers.Encoder(nout, nout * 2, 3, 2, 1)
@@ -16,15 +16,11 @@ class BaseNet(nn.Module):
         self.enc5 = layers.Encoder(nout * 6, nout * 8, 3, 2, 1)
 
         self.aspp = layers.ASPPModule(nout * 8, nout * 8, dilations, dropout=True)
-        self.lstm_aspp = layers.LSTMModule(nout * 8, min_size, nout_lstm)
 
-        self.dec4 = layers.Decoder(nout * (6 + 8) + 1, nout * 6, 3, 1, 1)
-
+        self.dec4 = layers.Decoder(nout * (6 + 8), nout * 6, 3, 1, 1)
         self.dec3 = layers.Decoder(nout * (4 + 6), nout * 4, 3, 1, 1)
-
         self.dec2 = layers.Decoder(nout * (2 + 4), nout * 2, 3, 1, 1)
-        self.lstm_dec2 = layers.LSTMModule(nout * 2, min_size * 8, nout_lstm)
-
+        self.lstm_dec2 = layers.LSTMModule(nout * 2, nin_lstm, nout_lstm)
         self.dec1 = layers.Decoder(nout * (1 + 2) + 1, nout * 1, 3, 1, 1)
 
     def __call__(self, x):
@@ -35,15 +31,11 @@ class BaseNet(nn.Module):
         e5 = self.enc5(e4)
 
         h = self.aspp(e5)
-        h = torch.cat([h, self.lstm_aspp(h)], dim=1)
 
         h = self.dec4(h, e4)
-
         h = self.dec3(h, e3)
-
         h = self.dec2(h, e2)
         h = torch.cat([h, self.lstm_dec2(h)], dim=1)
-
         h = self.dec1(h, e1)
 
         return h
@@ -55,22 +47,22 @@ class CascadedNet(nn.Module):
         super(CascadedNet, self).__init__()
         self.max_bin = n_fft // 2
         self.output_bin = n_fft // 2 + 1
-        self.min_size = self.max_bin // 16
+        self.nin_lstm = self.max_bin // 2
         self.offset = 64
 
         self.stg1_low_band_net = nn.Sequential(
-            BaseNet(2, 16, self.min_size // 2, 128),
+            BaseNet(2, 16, self.nin_lstm // 2, 128),
             layers.Conv2DBNActiv(16, 8, 1, 1, 0)
         )
-        self.stg1_high_band_net = BaseNet(2, 8, self.min_size // 2, 64)
+        self.stg1_high_band_net = BaseNet(2, 8, self.nin_lstm // 2, 64)
 
         self.stg2_low_band_net = nn.Sequential(
-            BaseNet(10, 32, self.min_size // 2, 128),
+            BaseNet(10, 32, self.nin_lstm // 2, 128),
             layers.Conv2DBNActiv(32, 16, 1, 1, 0)
         )
-        self.stg2_high_band_net = BaseNet(10, 16, self.min_size // 2, 64)
+        self.stg2_high_band_net = BaseNet(10, 16, self.nin_lstm // 2, 64)
 
-        self.stg3_full_band_net = BaseNet(26, 32, self.min_size, 128)
+        self.stg3_full_band_net = BaseNet(26, 32, self.nin_lstm, 128)
 
         self.out = nn.Conv2d(32, 2, 1, bias=False)
         self.aux_out = nn.Conv2d(24, 2, 1, bias=False)
